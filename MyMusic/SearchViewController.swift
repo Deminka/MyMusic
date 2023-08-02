a771fa981ae3f9d762fd2e208a7fdf530e20d914
Dmitry Komissarov <komdimkom@gmail.com>
2023-08-02 17:07:42 +0300

    create tabBarController

--- MyMusic/SearchViewController.swift
+++ MyMusic/SearchViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchViewController.swift
 //  MyMusic
 //
 //  Created by mac on 02.08.2023.
@@ -7,14 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class SearchViewController: UITableViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
     }
-
-
 }
-
